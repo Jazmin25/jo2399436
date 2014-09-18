8fe9f600c762fcf620d8297197068526d5b6000f
Jazmin Onofre <o.jazmin25@gmail.com>
2014-09-17 19:47:47 -0700

    Signed-off-by: Jazmin Onofre <o.jazmin25@gmail.com>

---
+++ simpleVec/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: jazmin
+ *
+ * Created on September 11, 2014, 1:27 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
